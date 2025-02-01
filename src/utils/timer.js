a4506a7225ef027745aae58f7ea95964d7189e2f
Margaux <margaux.tarayre@gmail.com>
2025-02-01 20:05:14 +0100

    add difficulty modes for timer duration

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,4 +1,9 @@
-export const MAX_TIME = 15;
+export const EASY_MAX_NUMBER = 15;
+export const HARD_MAX_NUMBER = 10;
+
+export const getMaxTime = (difficulty) => {
+  return difficulty === 'hard' ? HARD_MAX_NUMBER : EASY_MAX_NUMBER;
+}
 
 export const formatTime = (time) => {
   if (time < 10) {
